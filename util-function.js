c0e76dcddb5f0933e50c67c3ab2557d6f9970712
mynameishomin <mynameishomin@gmail.com>
2023-01-21 23:02:53 +0900

    add randomPick

--- util-function.js
+++ util-function.js
@@ -1,3 +1,14 @@
 const numberWithComma = (number) => {
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 };
+
+const randomPick = (arr, count, isOverlap) => {
+  const oldArr = [...arr];
+  const newArr = [];
+  for (let i = 0; i < count; i++) {
+    const randomNumber = Math.ceil(Math.random() * oldArr.length - 1);
+    newArr.push(oldArr[randomNumber]);
+    isOverlap ? null : oldArr.splice(randomNumber, 1);
+  }
+  return newArr;
+};
