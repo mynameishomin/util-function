1d6f6483ade3a45a395e0feef2ff7d6fa88f195b
mynameishomin <mynameishomin@gmail.com>
2023-01-23 19:02:09 +0900

    add getToday

--- util-function.js
+++ util-function.js
@@ -6,9 +6,20 @@ const randomPick = (arr, count, isOverlap) => {
   const oldArr = [...arr];
   const newArr = [];
   for (let i = 0; i < count; i++) {
+    if (!arr.length) break;
     const randomNumber = Math.ceil(Math.random() * oldArr.length - 1);
     newArr.push(oldArr[randomNumber]);
     isOverlap ? null : oldArr.splice(randomNumber, 1);
   }
   return newArr;
+};
+
+const getToday = (format) => {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth() + 1;
+  const date = today.getDate();
+  return `${year}-${month < 10 ? "0" + month : month}-${
+    date < 10 ? "0" + date : date
+  }`;
 };
