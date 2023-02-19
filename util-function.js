d718ba0bc80553c8e8fc3a16a052daac8eb8f673
mynameishomin <mynameishomin@gmail.com>
2023-02-19 15:02:30 +0900

    add reverseStr, isEmpty

--- util-function.js
+++ util-function.js
@@ -2,24 +2,15 @@ const numberWithComma = (number) => {
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 };
 
-const randomPick = (arr, count, isOverlap) => {
-  const oldArr = [...arr];
-  const newArr = [];
-  for (let i = 0; i < count; i++) {
-    if (!arr.length) break;
-    const randomNumber = Math.ceil(Math.random() * oldArr.length - 1);
-    newArr.push(oldArr[randomNumber]);
-    isOverlap ? null : oldArr.splice(randomNumber, 1);
-  }
-  return newArr;
+const reverseStr = (str) => {
+  return str.split("").reverse().join("");
 };
 
-const getToday = (format) => {
-  const today = new Date();
-  const year = today.getFullYear();
-  const month = today.getMonth() + 1;
-  const date = today.getDate();
-  return `${year}-${month < 10 ? "0" + month : month}-${
-    date < 10 ? "0" + date : date
-  }`;
+const isEmpty = (item) => {
+  if (typeof item === "object") {
+    return !Object.keys(item).length;
+  } else {
+    console.error("This is not Object");
+    return null;
+  }
 };
